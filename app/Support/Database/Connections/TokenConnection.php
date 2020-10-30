ecd9c7aae4bcaf99085a4caa18246086f6dabe29
Elcharitas <jonathanirhodia@gmail.com>
2020-10-30 13:05:16 +0100

    Fixes

--- app/Support/Database/Connections/TokenConnection.php
+++ app/Support/Database/Connections/TokenConnection.php
@@ -31,7 +31,6 @@ class TokenConnection {
      */
     public function __construct($key)
     {
-        
         if(false === $board = Board::Token($key)){
             //throw some error
         }
